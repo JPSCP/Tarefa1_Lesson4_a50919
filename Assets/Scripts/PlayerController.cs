8d8017e2a98cc66bd60f0115692a78830863da25
JPSCP <a50919@alunos.ipb.pt>
2026-05-14 11:14:42 +0100

    Lesson 4.3. 4-7 - PowerUp and CountDown
    4. Apply extra knockback with powerup
    5. Create Countdown Routine for powerup
    6. Add a powerup indicator
    7. Lesson Recap

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject powerupIndicator;
     private Rigidbody playerRb;
     private GameObject focalPoint;
-    public float speed = 5.0f;
+
     public bool hasPowerup;
+    public float speed = 5.0f;
+    private float powerupStrenght = 15.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +24,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3 (0, 0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,14 +33,27 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerup = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRotine());
+            powerupIndicator.SetActive(true);
         }
+    }
+
+    IEnumerator PowerupCountdownRotine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
-            Debug.Log("Collided with " + collision.gameObject.name + "with power up set to" + hasPowerup);
+            Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRigidBody.AddForce(awayFromPlayer * powerupStrenght, ForceMode.Impulse);
         }
     }
+
 }
